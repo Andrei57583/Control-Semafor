163ae7d20afd396b257945116ed218ead1cc9a21
dennismuturi-dev <md.dennismuturi@gmail.com>
2026-03-23 22:20:56 +0200

    Update tranzactie_iesire.sv

--- tranzactie_iesire.sv
+++ tranzactie_iesire.sv
@@ -1,39 +1,54 @@
 // Autor: Dennis Muturi
-
 `include "uvm_macros.svh"
 import uvm_pkg::*;
 
-`ifndef __rename_transaction
-`define __rename_transaction
+`ifndef __iesire_transaction
+`define __iesire_transaction
 
-//o tranzactie este formata din totalitatea datelor transmise la un moment dat pe o interfata
-class tranzactie_rename extends uvm_sequence_item;
-  
-  //componenta tranzactie se adauga in baza de date
-  `uvm_object_utils(tranzactie_rename)
-  
-  rand bit[2:0] addr;
-       bit      irq; //output, deci nu e rand
-  
-  
-  //constructorul clasei; această funcție este apelată când se creează un obiect al clasei "tranzactie"
-  function new(string name = "element_secventaa");//numele dat este ales aleatoriu, si nu mai este folosit in alta parte
-    super.new(name);  
-  	addr = 0;
-    irq  = 0;
+// O tranzactie_iesire reprezinta toate datele transmise la un moment dat pe o interfata
+
+class tranzactie_iesire extends uvm_sequence_item;
+
+    //componenta tranzactie se adauga in baza de date
+  `uvm_object_utils(tranzactie_iesire)
+
+  bit [2:0] masini;    // Masini: Red[2], Yellow[1], Green[0]
+  bit [1:0] pietoni;   // Pietoni: Red[1], Green[0]
+  bit       buzzer;    // Beeping sound
+  bit       lampa;     // Acknowledgement Light
+
+
+//constructorul clasei; această funcție este apelată când se creează un obiect al clasei "tranzactie"
+
+  function new(string name = "element_secventaa");
+    super.new(name);
+
+    masini  = 0;
+    pietoni = 0;
+    buzzer  = 0;
+    lampa   = 0;
+
   endfunction
-  
+
+     
   //functie de afisare a unei tranzactii
   function void afiseaza_informatia_tranzactiei();
-    $display("Valoarea adresei: %0h, IRQ: %b", addr, irq);
+
+    $display("--- STARE SEMAFOR ---");
+    $display("Masini: %b | Pietoni: %b | Buzzer: %b | Lampa: %b", masini, pietoni, buzzer, lampa);
+
   endfunction
-  
-  function tranzactie_rename copy();
-	copy = new();
-	copy.addr = this.addr;
-  copy.irq = this.irq;
-	return copy;
+
+  function tranzactie_iesire copy();
+    tranzactie_iesire cpy = new();
+    cpy.masini  = this.masini;
+    cpy.pietoni = this.pietoni;
+    cpy.buzzer  = this.buzzer;
+    cpy.lampa   = this.lampa;
+    return cpy;
+    
   endfunction
 
 endclass
+
 `endif
