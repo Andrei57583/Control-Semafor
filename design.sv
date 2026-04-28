8a05e4f3dce125fa3f00f98f5c9c12c817727f32
andreiflorea222 <147614440+andreiflorea222@users.noreply.github.com>
2026-04-28 17:36:37 +0300

    new
    buzzer

--- design.sv
+++ design.sv
@@ -13,7 +13,7 @@ module Semafor_control #(
 parameter start_intermitent_mode = 3,
 parameter stop_intermitent_mode  = 6,
 parameter blink_freq             = 24,
-parameter full_cycle             = 100 // timp incare butonul nu mai ia valori dupa apasarea butonului si dureaza pana cand rosu se schimba in verde semafor masina
+parameter full_cycle             = 8'd255 //256 // timp incare butonul nu mai ia valori dupa apasarea butonului si dureaza pana cand rosu se schimba in verde semafor masina
 )(
 
 input          clk         ,
@@ -38,27 +38,30 @@ output logic [3-1:0] semafor_masini, // MSB red, middle = yellow, LSB = green
 
 output logic [8-1:0] durata_display, //un indicator LED care arata masinilor cat timp mai au de asteptat
 output               lampa         , //lampa care ilumineaza trecerea de pietoni // activ pe senzor_lumina = 0
-output               buzer_pietoni  //buzer pentru persoane cu dizabilitati de vedere //activ pe pieton = 1
+output               buzzer_pietoni  //buzer pentru persoane cu dizabilitati de vedere //activ pe pieton = 1
 );
 
-task sgsggsggdg//////////////////////////////////////////////////////////////////////////////////////////////////
-//registri acceesibili prin APB
+//task sgsggsggdg//////////////////////////////////////////////////////////////////////////////////////////////////
+//registri acceesibili prin APB                  // pentru reconfigurare/ setari
 reg  [8-1:0]   car_green_reg;  //0
 reg  [8-1:0]   car_yellow_reg; //1
 reg  [8-1:0]   car_red_reg;    //2
 
-reg buton_pietoni_intarziat;
+//reg buton_pietoni_intarziat;
 wire buton_pietoni_apasat;
 //semnale interne
-//reg    pieton_semafor_reg,
 
-reg [blink_freq-1 : 0] interminent_counter;
-reg            [8-1:0] count ;   
+//reg    pieton_semafor_reg,
+reg pushed_button;
+
+reg [full_cycle -1:0] push_button_counter; // for after you push the button
+reg [blink_freq -1:0] interminent_counter; //counter for intermitent mode
+reg [full_cycle -1:0] cycle_counter ;   //full transition of the semaphore counter     //count = cycle_counter
 
 //scrierea registrilor prin APB
  always @(posedge clk or negedge rst_n)
     if (~rst_n) begin
-    count <= 'd0;
+  //  count <= 'd0;
     end
     else 
     if(Pwrite == 1 && Psel == 1 && Penable == 0)// suntem in primul tact al tranzactiei de scriere
@@ -106,7 +109,7 @@ always @(posedge clk or negedge rst_n)
 	  Pready <= 0;
 	else if(Psel == 1 && Penable == 0 )
 	Pready <= 'b1;
-endtask////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//endtask////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ========================= apasarea butonului si tranzitii =========================
 //assign buton_pietoni  = 'b0   ;
 //assign semafor_pieton = 'b10  ;
@@ -114,117 +117,106 @@ endtask/////////////////////////////////////////////////////////////////////////
 
 always@(posedge clk or negedge rst_n) 
 begin
-//tranzitii
-   if(rst_n)    //Set Counter to Zero
-      count <= 0;
-    else if(buton_apasat == 'b1)  //sem car red
-      count <= count + 1'b1;
-   // if (count == full_cycle)
-    //  count <= 0;
-   if(count > car_green_reg) //sem car green 
+//starting the cycle
+   if(rst_n)    
+      cycle_counter <= 0;
+    else if(push_button_counter == full_cycle - 1)  //sem car red // counter starts
+      cycle_counter <= cycle_counter + 1'b1;
+    if (cycle_counter == full_cycle)
+      cycle_counter <= 0;
+ 
+//tranzitii 
+   if(cycle_counter > car_green_reg) //sem car green 
 		semafor_masini <= 'b100;
     
-	if(count > car_yellow_reg) //sem car yellow  
+	if(cycle_counter > car_yellow_reg) //sem car yellow  
 		semafor_masini <= 'b010;
 		
-	if(count > car_red_reg) //sem car green
+	if(cycle_counter > car_red_reg) //sem car green
 		semafor_pietoni <= 'b10 ;
 		semafor_masini <= 'b001;
 	
   end
   
-  //counter descrescator, deoarece daca apasam butonul count numara doar pana la 1
-always@(posedge clk or negedge rst_n) 
+
+always@(posedge clk or negedge rst_n)
 begin
-//tranzitii
-   if(rst_n)    //Set Counter to Zero
-      counter <= 0;
-      
-      else // numara descrescator, cat timp numara count initial numara
-  
-  
-  
-  always@(posedge clk or negedge rst_n) 
-begin
-   if(rst_n)    //Set Counter to Zero
-      buton_pietoni_intarziat <= 0;
-      else
-      buton_pietoni_intarziat <= buton_pietoni;
-      
-      assign buton_pietoni_apasat = buton_pietoni && ~buton_pietoni_intarziat;
-  end
-  
-  /*
-    // ========================= dead buton after press =========================
-  always@(posedge clk) 
-begin
-  if(count != full_cycle || count != 0) 
-    buton <= 'b0;
-    
-   // assign Data = (buton == 1'b1) ? (car_yellow_reg || car_red_reg : {(n){1'bz}};
+  pushed_button <= buton_pietoni_apasat;
 end
+  assign buton_apasat = buton_pietoni_apasat & ~pushed_button;
+  
 
-  // ========================= display numbers =========================
-  always@(posedge clk) 
-  durata_display <= count;
-  end
+ 
 
+//
+//  // ========================= display numbers =========================
+//  always@(posedge clk) 
+//  durata_display <= count;
+//  end
+//
+//  
+//  // ========================= buzzer si lumini =========================
+//  if(car_green_reg != 0)
+//  buzer <= 'b1;
+//  
+//  if(senzor_lumina == 1) // senzor lumina
+//  lampa <= 'b1;
+//  
+//  // ========================= intermitent intre orele xx si XX =========================
+//  
+//    always@(posedge clk) 
+//  begin
+//  if(ora_curenta > start_intermitent_mode && ora_curenta < stop_intermitent_mode)
+//    
+//    always_ff @(posedge clk or posedge rst_n) begin
+//        if (rst_n) 
+//        begin
+//            interminent_counter <= 0;
+//            semafor_masini     <= 'b000;  // semafor off on all colors
+//        end else 
+//        begin
+//            interminent_counter <= interminent_counter + 1;
+//                                                                                                  //blink_freq-1 = max size of the register
+//            if (interminent_counter >=  (blink_freq-1)/2 && interminent_counter !(blink_freq-1)) // counter reaches middlepoint, yellow light on
+//                semafor_masini <= car_yellow_reg;                                               // counter reaches middlepoint, yellow light on
+//            if (interminent_counter == blink_freq-1)  //  counter overflows, yellow light off
+//                semafor_masini <= 'b000;
+//            
+//            //interminent_counter ====================================/2======================================
+//            //yellow color        off =============================off=on===================================on
+//        end
+//    end
+//  end
   
-  // ========================= buzzer si lumini =========================
-  if(car_green_reg != 0)
-  buzer <= 'b1;
-  
-  if(senzor_lumina == 1) // senzor lumina
-  lampa <= 'b1;
-  
-  // ========================= intermitent intre orele xx si XX =========================
-  
-    always@(posedge clk) 
-  begin
-  if(ora_curenta > start_intermitent_mode && ora_curenta < stop_intermitent_mode)
-    
-    always_ff @(posedge clk or posedge rst_n) begin
-        if (rst_n) 
-        begin
-            interminent_counter <= 0;
-            semafor_masini     <= 'b000;  // semafor off on all colors
-        end else 
-        begin
-            interminent_counter <= interminent_counter + 1;
-                                                                                                  //blink_freq-1 = max size of the register
-            if (interminent_counter >=  (blink_freq-1)/2 && interminent_counter !(blink_freq-1)) // counter reaches middlepoint, yellow light on
-                semafor_masini <= car_yellow_reg;                                               // counter reaches middlepoint, yellow light on
-            if (interminent_counter == blink_freq-1)  //  counter overflows, yellow light off
-                semafor_masini <= 'b000;
-            
-            //interminent_counter ====================================/2======================================
-            //yellow color        off =============================off=on===================================on
-        end
-    end
-  end
-  */
 endmodule
 
-//00000   1
-//00001   2 
-//00010   3
-//00011   4
-//00100   5
-//00101   7
-//00110   8
-//00111   9
-//01000  10
-//01001  11
-//01010  12
-//01011  13
-//01100  14
-//01101  15
-//01110  16
-//01111  17
-//10000  18
-//10001  19
-//10010  20
-//10011  21
-//10100  22
-//10101  23
-//10110  24
+  //counter descrescator, deoarece daca apasam butonul count numara doar pana la 1, porneste counterul care tine butonul in 1
+//always@(posedge clk or negedge rst_n) 
+//begin
+//   if(buton_pietoni)  
+//    pushed_button_counter <= full_cycle;
+//    if(pushed_button_counter != 0'b1)
+//      pushed_button_counter <= pushed_button_counter - 1'b1;
+//end
+
+
+  //
+//always@(posedge clk or negedge rst_n) 
+//begin
+//   if(rst_n)    //Set Counter to Zero
+//      buton_pietoni_intarziat <= 0;
+//      else
+//      buton_pietoni_intarziat <= buton_pietoni;
+//      
+//      assign buton_pietoni_apasat = buton_pietoni && ~buton_pietoni_intarziat;
+//  end
+
+
+//    // ========================= dead buton after press =========================
+//  always@(posedge clk) 
+//begin
+//  if(count != full_cycle || count != 0) 
+//    buton <= 'b0;
+//    
+//   // assign Data = (buton == 1'b1) ? (car_yellow_reg || car_red_reg : {(n){1'bz}};
+//end
